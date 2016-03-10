61e140561ce6abbb73ec9abe5a29bddc82acb055
Sebastian Dimunzio <sdimunzio@gmail.com>
2016-03-10 17:11:40 -0300

    removing merge code

--- esri-ui/src/components/layout/landing.jsx
+++ esri-ui/src/components/layout/landing.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import { Link  } from 'react-router';
-<<<<<<< HEAD
-import {Message} from 'app/components/lan/LanContainer';
-import Map2d from 'app/components/map2d'
-=======
-import {Message} from 'app/components/lan/container';
 
->>>>>>> 7f33a67cc4b03662778b592de8fb9bc754aa8c2e
 export default class Landing extends React.Component {
 
   constructor() {
