00d8ea158e755b7dc2e78fe7866e86884f755a05
Daniel Oliva Bianco <dbianco@developmentgateway.org>
2016-03-29 09:22:25 -0300

    Add missing sql

---
+++ standalone/src/main/resources/sql/basic-data/indicator.sql
@@ -0,0 +1,10 @@
+DELETE FROM indicator;
+
+INSERT INTO indicator (id, name, description, unit) VALUES (1, 'Total Employment 2014', 'http://countrystat.psa.gov.ph/', 'persons');
+
+INSERT INTO indicator (id, name, description, unit) VALUES (2, 'Housing Population 2010', 'http://www.census.gov.ph/', 'persons');
+
+
+--http://www.census.gov.ph/content/literacy-status-filipinos-results-2003-functional-literacy-education-and-mass-media-survey
+
+--http://www.census.gov.ph/content/education-women-and-men
