e50a1367341de1e6d3d515fa002e98f7e0debae2
Daniel Oliva Bianco <dbianco@developmentgateway.org>
2016-04-12 19:45:48 -0300

    Add PhysicalStatus chart, filter and update the other endpoints

---
+++ persistence/src/main/java/org/devgateway/geoph/persistence/repository/PhysicalStatusRepository.java
@@ -0,0 +1,21 @@
+package org.devgateway.geoph.persistence.repository;
+
+import org.devgateway.geoph.model.PhysicalStatus;
+import org.devgateway.geoph.util.Parameters;
+
+import java.util.List;
+
+/**
+ * @author dbianco
+ *         created on abr 12 2016.
+ */
+public interface PhysicalStatusRepository {
+
+    List<PhysicalStatus> findAll();
+
+    PhysicalStatus findByName(String name);
+
+    PhysicalStatus findByCode(String code);
+
+    List<Object> findFundingByPhysicalStatus(Parameters params);
+}
