6825f890fb8d8d5e3d76fafc1893c517d0ecac03
mbertoli <mfbertoli@gmail.com>
2016-07-01 16:00:07 -0300

    missed file

--- leaflet-ui/app/scripts/components/charts/chartComponent.jsx
+++ leaflet-ui/app/scripts/components/charts/chartComponent.jsx
@@ -93,10 +93,10 @@ export default class ChartComponent extends React.Component {
 				},
 				//'autosize': true,
 				'legend':{
-					x:1.5,
+					x:-0.5,
 					y:1,
-					xanchor:"right",
-					yanchor:"auto",
+					xanchor:"left",
+					yanchor:"top",
 					bgcolor:"rgba(0, 0, 0, 0)",
 					font:{
 						size:10
