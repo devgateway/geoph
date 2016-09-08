f4eeaac8cfa4b5fecc552012cefdd752352bc192
mbertoli <mfbertoli@gmail.com>
2016-09-08 20:05:26 -0300

    N/A

---
+++ entity-model/src/main/java/org/devgateway/geoph/model/ProjectClimateChangeId.java
@@ -0,0 +1,71 @@
+package org.devgateway.geoph.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+/**
+ * @author dbianco
+ *         created on ago 22 2016.
+ */
+@Embeddable
+public class ProjectClimateChangeId implements Serializable {
+
+    @JsonIgnore
+    private Project project;
+
+    private ClimateChange climateChange;
+
+    public ProjectClimateChangeId(){}
+
+    public ProjectClimateChangeId(Project project, ClimateChange climateChange) {
+        this.project = project;
+        this.climateChange = climateChange;
+    }
+
+    @ManyToOne
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    @ManyToOne
+    public ClimateChange getClimateChange() {
+        return climateChange;
+    }
+
+    public void setClimateChange(ClimateChange climateChange) {
+        this.climateChange = climateChange;
+    }
+
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ProjectClimateChangeId that = (ProjectClimateChangeId) o;
+
+        if (project != null ? !project.equals(that.project) : that.project != null) {
+            return false;
+        }
+        if (climateChange != null ? !climateChange.equals(that.climateChange) : that.climateChange != null){
+            return false;
+        }
+
+        return true;
+    }
+
+    public int hashCode() {
+        int result;
+        result = (project != null ? project.hashCode() : 0);
+        result = 31 * result + (climateChange != null ? climateChange.hashCode() : 0);
+        return result;
+    }
+}
---
+++ entity-model/src/main/java/org/devgateway/geoph/model/ProjectGenderResponsivenessId.java
@@ -0,0 +1,71 @@
+package org.devgateway.geoph.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+/**
+ * @author dbianco
+ *         created on ago 22 2016.
+ */
+@Embeddable
+public class ProjectGenderResponsivenessId implements Serializable {
+
+    @JsonIgnore
+    private Project project;
+
+    private GenderResponsiveness gender_responsiveness;
+
+    public ProjectGenderResponsivenessId(){}
+
+    public ProjectGenderResponsivenessId(Project project, GenderResponsiveness genderResponsiveness) {
+        this.project = project;
+        this.gender_responsiveness = genderResponsiveness;
+    }
+
+    @ManyToOne
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    @ManyToOne
+    public GenderResponsiveness getGender_responsiveness() {
+        return gender_responsiveness;
+    }
+
+    public void setGender_responsiveness(GenderResponsiveness gender_responsiveness) {
+        this.gender_responsiveness = gender_responsiveness;
+    }
+
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ProjectGenderResponsivenessId that = (ProjectGenderResponsivenessId) o;
+
+        if (project != null ? !project.equals(that.project) : that.project != null) {
+            return false;
+        }
+        if (gender_responsiveness != null ? !gender_responsiveness.equals(that.gender_responsiveness) : that.gender_responsiveness != null){
+            return false;
+        }
+
+        return true;
+    }
+
+    public int hashCode() {
+        int result;
+        result = (project != null ? project.hashCode() : 0);
+        result = 31 * result + (gender_responsiveness != null ? gender_responsiveness.hashCode() : 0);
+        return result;
+    }
+}
