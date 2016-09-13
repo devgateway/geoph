77ab9d5da631d5944d893d91ca04400f6bf8a4af
mbertoli <mfbertoli@gmail.com>
2016-09-13 15:22:37 -0300

    N/A

--- leaflet-ui/app/scripts/reducers/map.js
+++ leaflet-ui/app/scripts/reducers/map.js
@@ -261,7 +261,7 @@ const onToggleLayer=(state,action)=>{
 
 const onSetSetting=(state,action)=>{
   var {id, name, value} = action;
-  
+  debugger;
   return state.setIn(getPath(id, ["settings", name]), value);
 }
 
