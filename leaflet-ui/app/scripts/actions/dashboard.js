28f73038d8674baf3d1a633f48d2a9ef34b0f532
mbertoli <mfbertoli@gmail.com>
2016-09-15 16:01:16 -0300

    GEOPH-594

--- leaflet-ui/app/scripts/actions/dashboard.js
+++ leaflet-ui/app/scripts/actions/dashboard.js
@@ -21,7 +21,9 @@ const redirect=(url,messages,errors,httpError)=>{
 export const getMapList =(params)=>{
   Object.assign(params, {type:"save"})
   return (dispatch, getState) =>{
-	 	Connector.getMapList(params).then((data)=>dispatch( {type: Constants.REQUEST_MAP_LIST_OK,data}));
+	 	Connector.getMapList(params).then((data)=>{
+      dispatch( {type: Constants.REQUEST_MAP_LIST_OK,data})
+    });
 	}
 }
 
@@ -36,7 +38,7 @@ export const edit =(key)=>{
 export const remove=(key)=>{
   return (dispatch, getState) =>{
     Connector.removeDashboard(key).then((data)=>{
-       dispatch(getMapList());
+       dispatch({type: Constants.REQUEST_DELETE_MAP_OK, key});
        dispatch(redirect('/dashboard',[`Dashboard was removed`]));
     }).catch((httpError)=>{
       dispatch(redirect('/dashboard',[],[],httpError));
