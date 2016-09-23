35d4c0b88721ea47c57f862fa8175f7f1fb9cfcc
mbertoli <mfbertoli@gmail.com>
2016-09-23 15:37:59 -0300

    GEOPH-619

--- leaflet-ui/app/scripts/components/controls/settings.jsx
+++ leaflet-ui/app/scripts/components/controls/settings.jsx
@@ -12,51 +12,49 @@ const Settings =React.createClass({
     return {'showSettings': false};
   },
 
-  setFunding(ev){
-    this.props.onSetFundingType(ev.target.value);
-    this.setState({'fundingType': ev.target.value});
-  },
-
   changeFundingMeasure(ev){
-    this.props.onSetFundingType({measure: ev.target.value, type: this.props.fundingType.type});
+    let measure = ev.target.value;
+    if (measure=='commitments'){
+      this.props.onSetFundingType({measure, type: 'actual'});
+    } else {
+      this.props.onSetFundingType({measure, type: this.props.fundingType.type});
+    }
   },
 
   changeFundingType(ev){
     this.props.onSetFundingType({measure: this.props.fundingType.measure, type: ev.target.value});
   },
 
- 
   render() {
-    const {visible}=this.props;
-
+    const {visible, fundingType={}} = this.props;
+    const {measure, type} = fundingType;
     return (
      <div>
         {visible?
-           
-      
           <div className="settings-container">
             <h2>{translate('header.settings.fundingtype')}</h2>
            
-           <div className="list">
+           <div className="list"> 
             <div className="setting">
-                <select className='form-control input-sm' value={this.props.fundingType.measure} onChange={this.changeFundingMeasure}>
-                  <option value='commitments'>{translate('header.settings.commitments')}</option>
-                  <option value='disbursements'>{translate('header.settings.disbursements')}</option>
-                  <option value='expenditures'>{translate('header.settings.expenditures')}</option>
-                </select>
-           
-              </div>   
-                 <div className="setting">
-                <select className='form-control input-sm' value={this.props.fundingType.type} onChange={this.changeFundingType}>
+                <select className='form-control input-sm' value={type} onChange={this.changeFundingType}>
                   <option value='actual'>{translate('header.settings.actual')}</option>
-                  <option value='cancelled'>{translate('header.settings.cancelled')}</option>
-                  <option value='target'>{translate('header.settings.target')}</option>
+                  {measure!='commitments'?
+                    <option value='cancelled'>{translate('header.settings.cancelled')}</option>
+                  : null}  
+                  {measure!='commitments'?
+                    <option value='target'>{translate('header.settings.target')}</option>
+                  : null}
                 </select>
               </div>            
             </div> 
+            <div className="setting">
+              <select className='form-control input-sm' value={measure} onChange={this.changeFundingMeasure}>
+                <option value='commitments'>{translate('header.settings.commitments')}</option>
+                <option value='disbursements'>{translate('header.settings.disbursements')}</option>
+                <option value='expenditures'>{translate('header.settings.expenditures')}</option>
+              </select>
+            </div>  
           </div>
-
-
         : null}
       </div>
     );
