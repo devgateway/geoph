2517287d19086fddcbc41b7a37d4108fb0c420f5
Daniel Oliva Bianco <dbianco@developmentgateway.org>
2016-09-26 15:55:14 -0300

    GEOPH-623 - Re-labeling in Filters

--- standalone/src/main/resources/sql/basic-data/flowType.sql
+++ standalone/src/main/resources/sql/basic-data/flowType.sql
@@ -2,4 +2,4 @@ DELETE FROM flow_type;
 
 INSERT INTO flow_type (id,name) VALUES (1,'Loan');
 INSERT INTO flow_type (id,name) VALUES (2,'Grant');
-INSERT INTO flow_type (id,name) VALUES (3,'PMC');
+INSERT INTO flow_type (id,name) VALUES (3,'Locally Funded Projects and Programs');
