9ee956cf727d0656be5d9423660e5cf3a0b62c07
Daniel Oliva Bianco <dbianco@developmentgateway.org>
2016-11-16 14:14:10 -0300

    Release 1.3

--- standalone/src/main/scripts/run.bat
+++ standalone/src/main/scripts/run.bat
@@ -1,1 +1,1 @@
-java -DCONF_PATH=./  -DLOG_PATH=/var/log/geoph  -Xms1G -Xmx2G -XX:+UseConcMarkSweepGC -jar geoph-api-standalone-1.3-SNAPSHOT.jar
+java -DCONF_PATH=./  -DLOG_PATH=/var/log/geoph  -Xms1G -Xmx2G -XX:+UseConcMarkSweepGC -jar geoph-api-standalone-1.4-SNAPSHOT.jar
