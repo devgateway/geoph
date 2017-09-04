a0c129eb402f24689276d0772f92bcf2fb49fb62
Ionut Dobre <idobre@developmentgateway.org>
2017-09-04 14:50:27 +0300

    checkstyle.

--- leaflet-ui/app/scripts/util/saveUtil.js
+++ leaflet-ui/app/scripts/util/saveUtil.js
@@ -1,99 +1,103 @@
 import {getVisibles, getPath, plainList} from './layersUtil';
 import translate from './translate';
 
-const collect=(options)=>{
-  let values=[];
+const collect = (options) => {
+  let values = [];
   //first level iteration
-  options.forEach((item)=>{
-    if (item.selected){
+  options.forEach((item) => {
+    if (item.selected) {
       values.push(item.id); //use values.push(item.name);  for debug purpose instead of id
     }
-    if (item.items){ //next levels iterations
-      let nested=collect(item.items);
-      values=values.concat(nested);
+    if (item.items) { //next levels iterations
+      let nested = collect(item.items);
+      values = values.concat(nested);
     }
   });
   return values;
-}
+};
 
-const collectRange=(options)=>{
+const collectRange = (options) => {
   return {'minSelected': options.minSelected, 'maxSelected': options.maxSelected};
-}
+};
 
-
-const getBgColorFromCssName=(cls)=>{
-  var div = document.createElement("div");
+const getBgColorFromCssName = (cls) => {
+  const div = document.createElement("div");
   div.style.display = "none";
   document.body.appendChild(div);
   div.className = cls;
   
-  var color=getComputedStyle(div).getPropertyValue("background-color");
+  const color = getComputedStyle(div).getPropertyValue("background-color");
   div.parentNode.removeChild(div)
-  var matchColors = /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/;
-  var match = matchColors.exec(color);
+  const matchColors = /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/;
+  const match = matchColors.exec(color);
   if (match !== null) {
-    return {r: match[1],g:match[2] ,b:match[3]};
+    return {r: match[1], g: match[2], b: match[3]};
   }
   
   return null;
-}
+};
 
 
-export const collectValuesToSave = (state)=>{
-  console.log('collectValuesToSave');
-  let filters=state.filters.filterMain;
-  let projectSearch=state.projectSearch;
-  let map=state.map;
-  let settings=state.settings;
-  let params={};
-  let filterParams={};
+export const collectValuesToSave = (state) => {
+  let filters = state.filters.filterMain;
+  let projectSearch = state.projectSearch;
+  let map = state.map;
+  let settings = state.settings;
+  let params = {};
+  let filterParams = {};
   let selection;
-  for(let param in filters){
-    let options=filters[param].items;
-    if (filters[param].isRange){
-      selection=collectRange(filters[param]);
-      if(selection.minSelected){
-        filterParams[param+'_min']=selection.minSelected;
+  
+  for (let param in filters) {
+    let options = filters[param].items;
+    if (filters[param].isRange) {
+      selection = collectRange(filters[param]);
+      if (selection.minSelected) {
+        filterParams[param + '_min'] = selection.minSelected;
       }
-      if(selection.maxSelected){
-        filterParams[param+'_max']=selection.maxSelected;
+      if (selection.maxSelected) {
+        filterParams[param + '_max'] = selection.maxSelected;
       }
     } else {
-      selection=collect(options);
-      if(selection.length > 0){
-        filterParams[param]=selection;
+      selection = collect(options);
+      if (selection.length > 0) {
+        filterParams[param] = selection;
       }
     }
   }
-  if (projectSearch){
+  
+  if (projectSearch) {
     let idsSelected = [];
     projectSearch.selected.map(it => idsSelected.push(it.id));
     Object.assign(filterParams, {'pr': idsSelected});
   }
+  
   Object.assign(params, {'filters': filterParams});
-  if(map){
+  
+  if (map) {
     let layers = getVisibles(map.get('layers')).toJS();
     let visibleLayers = [];
-    layers.map((layer)=>{
+    layers.map((layer) => {
       const {legends, name, keyName, id} = layer;
-      let nameLabel =  keyName? translate("toolview.layers."+keyName) : name;
+      let nameLabel = keyName ? translate("toolview.layers." + keyName) : name;
       
-      var coloredLegend=legends.map((l)=>{
-        const {cls}=l;
-        const color=getBgColorFromCssName(cls)
-        return {...l,color}
-      })
-      visibleLayers.push({id, name: nameLabel, legends:coloredLegend});
-    })
+      const coloredLegend = legends.map((l) => {
+        const {cls} = l;
+        const color = getBgColorFromCssName(cls)
+        return {...l, color}
+      });
+      visibleLayers.push({id, name: nameLabel, legends: coloredLegend});
+    });
     Object.assign(params, {'visibleLayers': visibleLayers});
   }
+  
   let layersPlain = plainList(map.get('layers'));
-  layersPlain.forEach(function(l){
+  layersPlain.forEach(function (l) {
     map = map.setIn(getPath(l.get('id'), ['data']), {});
-  })
-  map = map.set('defaultBounds', map.get('bounds'));//move bounds value to defaultBounds to be used as default on restore
+  });
+  
+  map = map.set('defaultBounds', map.get('bounds')); //move bounds value to defaultBounds to be used as default on restore
   Object.assign(params, {'map': map});
   Object.assign(params, {'settings': settings});
   
   return params;
-}
+};
