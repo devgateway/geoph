b4e7370ddfc6b70ecacb3c769f1bfce4e1251b83
Ionut Dobre <idobre@developmentgateway.org>
2017-09-06 13:18:14 +0300

    checkstyle.

--- leaflet-ui/app/scripts/components/controls/print.jsx
+++ leaflet-ui/app/scripts/components/controls/print.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import {capture}  from '../../actions/capture.js';
+import {connect} from 'react-redux'
+import {capture} from '../../actions/capture.js';
 import Settings from '../../util/settings';
 
 require('./print.scss');
 
-const Share =React.createClass({
+const Share = React.createClass({
   render() {
-    const {onCapure,loading,captures,visible}=this.props;
+    const {onCapure, loading, captures, visible} = this.props;
     return (
       
       <div>
-        {visible?
+        {visible ?
           <div className="print-container">
-            <div className="new_loading">{loading?<img src="../../../assets/png/loading.gif"/>:
-              <div  className="new"><div className="icon" onClick={onCapure}></div><span>create</span></div>}</div>
-            <span c className={captures.length>0?"small":"big"}>Click on create icon to generate a pdf of current map</span>
-            {captures.map((file)=><a target="_blank" href={Settings.get('API','PDF_DOWNLOAD') + file.name}><div className="icon"></div><span>{"Pdf# "+(file.count)}</span></a>)}
+            <div className="new_loading">{loading ? <img src="../../../assets/png/loading.gif"/> :
+              <div className="new">
+                <div className="icon" onClick={onCapure}></div>
+                <span>create</span></div>}</div>
+            <span c className={captures.length > 0 ? "small" : "big"}>Click on create icon to generate a pdf of current map</span>
+            {captures.map((file) => <a target="_blank" href={Settings.get('API', 'PDF_DOWNLOAD') + file.name}>
+              <div className="icon"></div>
+              <span>{"Pdf# " + (file.count)}</span></a>)}
           </div>
           : null}
       </div>
@@ -26,7 +30,7 @@ const Share =React.createClass({
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    onCapure:_=>dispatch(capture())
+    onCapure: _ => dispatch(capture())
   }
 };
 
