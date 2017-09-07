19f83bf88c4910b66651ef6bed9cd0711cb373ae
Ionut Dobre <idobre@developmentgateway.org>
2017-09-07 15:39:49 +0300

    GEOPH-676 - add option to export left of right map.

--- leaflet-ui/app/scripts/components/controls/export.jsx
+++ leaflet-ui/app/scripts/components/controls/export.jsx
@@ -11,9 +11,24 @@ class Export extends React.Component {
     isCompare:   React.PropTypes.bool,
   };
   
-  getDownloadURL(type) {
-    const { filters, projectSearch } = this.props;
-    let fc = collectValues(filters, projectSearch);
+  getDownloadURL(type, options) {
+    const { isCompare, filters, projectSearch, filtersCompare, projectSearchCompare } = this.props;
+    let fc;
+    
+    if (!isCompare) {
+      fc = collectValues(filters, projectSearch);
+    } else {
+      if (options !== undefined) {
+        // only export the main map
+        if (options.right === true) {
+          fc = collectValues(filters, projectSearch);
+        }
+        // only export the comparison map
+        if (options.left === true) {
+          fc = collectValues(filtersCompare, projectSearchCompare);
+        }
+      }
+    }
     
     return Connector.getExportURL(type, fc);
   };
@@ -42,23 +57,23 @@ class Export extends React.Component {
                 
                 : <div>
                   <div className="export-csv compare">
-                    <a target="_blank" href={this.getDownloadURL('csv')}>
+                    <a target="_blank" href={this.getDownloadURL('csv', {left: true})}>
                       <Button className="btn btn-xs" bsStyle='success'>Download Left Map data in <strong>CSV</strong> format</Button>
                     </a>
                   </div>
                   <div className="export-xls compare">
-                    <a target="_blank" href={this.getDownloadURL('xls')}>
+                    <a target="_blank" href={this.getDownloadURL('xls', {left: true})}>
                       <Button className="btn btn-xs" bsStyle='success'>Download Left Map data in <strong>XLS</strong> format</Button>
                     </a>
                   </div>
                   
                   <div className="export-csv compare">
-                    <a target="_blank" href={this.getDownloadURL('csv')}>
+                    <a target="_blank" href={this.getDownloadURL('csv', {right: true})}>
                       <Button className="btn btn-xs" bsStyle='success'>Download Right Map data in <strong>CSV</strong> format</Button>
                     </a>
                   </div>
                   <div className="export-xls compare">
-                    <a target="_blank" href={this.getDownloadURL('xls')}>
+                    <a target="_blank" href={this.getDownloadURL('xls', {right: true})}>
                       <Button className="btn btn-xs" bsStyle='success'>Download Right Map data in <strong>XLS</strong> format</Button>
                     </a>
                   </div>
@@ -73,11 +88,17 @@ class Export extends React.Component {
 
 const mapStateToProps = (state, props) => {
   const isCompare = state.compare.size !== 0;
-  
   return {
     lang: state.language.lan,
+    
+    // filters and project search for the main map
     filters: state.filters.filterMain,
     projectSearch: state.projectSearch,
+    
+    // filters and project search for the compare map
+    filtersCompare: state.compare.get("filters") !== undefined ? state.compare.get("filters").filterMain : undefined,
+    projectSearchCompare: state.compare.get("projectSearch"),
+    
     isCompare
   };
 };
