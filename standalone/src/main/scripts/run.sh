5804244d719e26dd179988c549c571c597c0a19f
dbianco <dbianco@developmentgateway.org>
2017-09-08 10:21:57 -0300

    Release version 1.5

--- standalone/src/main/scripts/run.sh
+++ standalone/src/main/scripts/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -DCONF_PATH=/opt/geoph/geoph-api  -DLOG_PATH=/opt/geoph/geoph-api   -Xms1G -Xmx2G -XX:+UseConcMarkSweepGC -jar /opt/geoph/geoph-api/geoph-api-standalone-1.4-SNAPSHOT.jar
+java -DCONF_PATH=/opt/geoph/geoph-api  -DLOG_PATH=/opt/geoph/geoph-api   -Xms1G -Xmx2G -XX:+UseConcMarkSweepGC -jar /opt/geoph/geoph-api/geoph-api-standalone-1.6-SNAPSHOT.jar
