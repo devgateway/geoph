bbbc569cfa68b5f1f227210e73713967e75201dd
Ionut Dobre <idobre@developmentgateway.org>
2017-09-08 17:25:42 +0300

    checkstyle.

--- leaflet-ui/app/scripts/reducers/projectSearch.js
+++ leaflet-ui/app/scripts/reducers/projectSearch.js
@@ -9,11 +9,11 @@ const projectSearch = (state = {'selected': [], 'applied': [], 'results': {}, 'k
       stateCloned = cloneDeep(state);
       let itemIndex = -1
       stateCloned.selected.forEach((item, index) => {
-        if (item.id == action.project.id){
+        if (item.id == action.project.id) {
           itemIndex = index;
         }
       })
-      if (itemIndex!=-1){
+      if (itemIndex != -1) {
         stateCloned.selected.splice(itemIndex, 1);
       } else {
         stateCloned.selected.push(action.project);
@@ -23,43 +23,44 @@ const projectSearch = (state = {'selected': [], 'applied': [], 'results': {}, 'k
       stateCloned = cloneDeep(state);
       stateCloned.results.content.forEach((item) => {
         let isAlreadyAdded = false;
-        stateCloned.selected.forEach((it) => {it.id==item.id? isAlreadyAdded=true : null;});
-        if (!isAlreadyAdded){
+        stateCloned.selected.forEach((it) => {
+          it.id == item.id ? isAlreadyAdded = true : null;
+        });
+        if (!isAlreadyAdded) {
           stateCloned.selected.push(item);
         }
       })
       return stateCloned;
     case Constants.STATE_RESTORE:
       stateCloned = cloneDeep(state);
-      action.storedMap.data.filters['pr'].forEach(e=>{
+      action.storedMap.data.filters['pr'].forEach(e => {
         stateCloned.selected.push({id: e});
-      }); 
+      });
       return stateCloned;
-
+    
     case Constants.APPLY_PROJECT_SELECTED:
       stateCloned = cloneDeep(state);
       Object.assign(stateCloned, {'applied': state.selected});
       return stateCloned;
-
+    
     case Constants.CLEAR_ALL_PROJECT_SELECTED:
       stateCloned = cloneDeep(state);
-      Object.assign(stateCloned, {'selected':[]});
-      Object.assign(stateCloned, {'applied':[]});
+      Object.assign(stateCloned, {'selected': []});
+      Object.assign(stateCloned, {'applied': []});
       return stateCloned;
-
+    
     case Constants.CLEAR_ALL_RESULTS:
       stateCloned = cloneDeep(state);
-      Object.assign(stateCloned, {'keyword':''});
+      Object.assign(stateCloned, {'keyword': ''});
       Object.assign(stateCloned, {'results': {}});
       return stateCloned;
-
+    
     case Constants.REQUEST_PROJECT_BY_TEXT:
       projectSearchResults = {isFetching: true};
       return Object.assign({}, state, {'results': projectSearchResults});
     case Constants.SET_KEYWORD:
       return Object.assign({}, state, {'keyword': action.keyword});
     case Constants.RECEIVE_PROJECT_BY_TEXT:
-      console.log('projectSearch reducer')
       projectSearchResults = {lastUpdate: action.receivedAt, isFetching: false};
       Object.assign(projectSearchResults, action.data);
       return Object.assign({}, state, {'results': projectSearchResults});
